90b2344525643814ea897177da515455432d3d17
lancerdonnie <jideadedejifirst@gmail.com>
2019-09-16 19:31:52 +0100

    added some refactoring

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 $(
     () => {
         var $list = $("#list")
+        function app(appe) {
+            $list.append(
+                `<li>${appe["id"]})  ${appe["question"]}</li> 
+                <p>${appe["option1"]}</p> 
+                <p>${appe["option2"]}</p> 
+                <p>${appe["option3"]}</p> 
+                <p>${appe["option4"]}</p>
+                <button>answer</button>
+                <button class="remove" data-id=${appe.id}>delete</button>`)
+        }
         $("#button1").click(() => {
             let obj = {
-                $question: $("#question").val(),
-                $option1: $("#option1").val(),
-                $option2: $("#option2").val(),
-                $option3: $("#option3").val(),
-                $option4: $("#option4").val(),
-                $answer: $("#answer").val()
+                question: $("#question").val(),
+                option1: $("#option1").val(),
+                option2: $("#option2").val(),
+                option3: $("#option3").val(),
+                option4: $("#option4").val(),
+                answer: $("#answer").val()
             }
             $.post(
                 "http://localhost:3000/questions",
                 obj,
                 (data, status) => {
-                    $list.append($(
-                        `<li>${data["id"]})  ${data["question"]}</li> 
-                        <p>${data["option1"]}</p> 
-                        <p>${data["option2"]}</p> 
-                        <p>${data["option3"]}</p> 
-                        <p>${data["option4"]}</p>
-                        <button>answer</button>
-                        <button class="remove${data["id"]}" data-id=${data["id"]}>delete</button>`))
-
+                    app(data)
                 }
             )
 
@@ -31,32 +33,21 @@ $(
             "http://localhost:3000/questions",
             (data, status) => {
                 $.each(data, (i, e) => {
-                    $list.append(
-                        `<li>${data[i]["id"]})  ${data[i]["question"]}</li>
-                        <p>${data[i]["option1"]}</p> 
-                        <p>${data[i]["option2"]}</p> 
-                        <p>${data[i]["option3"]}</p> 
-                        <p>${data[i]["option4"]}</p>
-                        <button >answer</button>
-                        <button class="remove${data[i]["id"]}" data-id=${data[i]["id"]}>delete</button> 
-                       `)
-                       
+                    app(e)
                 })
             }
         )//total list
-
-       
-        // $list.delegate(".remove","click",() => {
-        // $(".remove").click(() => {
-        //     $.ajax({
-        //         // url: "http://localhost:3000/questions/"+$(this).attr("data-id"),
-        //         url: "http://localhost:3000/questions/"+,
-        //         type: "DELETE",
-        //         success: (result) => {
-        //             //add a successfully deleted line
-        //         }
-        //     })
-        // })//remove element
+        $list.delegate(".remove", "click", () => {
+                console.log($(this).attr("data-id"))
+                $.ajax({
+                    url: "http://localhost:3000/questions/"+$(this).attr("data-id"),
+                    // url: "http://localhost:3000/questions/" +,
+                    type: "DELETE",
+                    success: (result) => {
+                        //add a successfully deleted line
+                    }
+                })
+        })//remove element
 
         $(".ans").click(() => {
             $.ajax({
