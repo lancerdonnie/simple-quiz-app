37f2a1fe7dbff5ff72480bb72ab4ce65686990c0
lancerdonnie <jideadedejifirst@gmail.com>
2019-09-17 16:45:56 +0100

    added update and delete functionality

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 $(
     () => {
         var $list = $("#list")
-        function app(appe) {
+        var total=0;
+        var app = function (appe) {
+
             $list.append(
-                `<li>${appe["id"]})  ${appe["question"]}</li> 
-                <p>${appe["option1"]}</p> 
-                <p>${appe["option2"]}</p> 
-                <p>${appe["option3"]}</p> 
-                <p>${appe["option4"]}</p>
-                <button>answer</button>
-                <button class="remove" data-id=${appe.id}>delete</button>`)
+                `<li class="def${appe["id"]}">${appe["id"]})  ${appe["question"]}
+                <input id="radio1" name="radio${appe["id"]}" type="radio" value="on">${appe["option1"]}</input> 
+                <input id="radio2" name="radio${appe["id"]}" type="radio" value="on">${appe["option2"]}</input> 
+                <input id="radio3" name="radio${appe["id"]}" type="radio" value="on">${appe["option3"]}</input> 
+                <input id="radio4" name="radio${appe["id"]}" type="radio" value="on">${appe["option4"]}</input>
+                <button class="ans" daid="${appe["id"]}">answer</button>
+                <button class="remove" data-id="${appe["id"]}">X</button></li>`)
         }
+        
+        $("#button2").click(() => {
+            let obj = {
+                id: $("#idd").val(),
+                question: $("#question").val(),
+                option1: $("#option1").val(),
+                option2: $("#option2").val(),
+                option3: $("#option3").val(),
+                option4: $("#option4").val(),
+                answer: $("#answer").val()
+            }
+            $.ajax({
+                url: `http://localhost:3000/questions/${obj.id}`,
+                type: "PUT",
+                data: obj,
+                async: true,
+                success: (data) => {
+
+                    app(data)
+                }
+            })
+        })//update
         $("#button1").click(() => {
             let obj = {
                 question: $("#question").val(),
@@ -20,45 +44,84 @@ $(
                 option4: $("#option4").val(),
                 answer: $("#answer").val()
             }
-            $.post(
-                "http://localhost:3000/questions",
-                obj,
-                (data, status) => {
+            $.ajax({
+                url: "http://localhost:3000/questions",
+                type: "POST",
+                data: obj,
+                async: true,
+                success: (data) => {
                     app(data)
                 }
-            )
-
+            })
         })//submit
-        $.get(
-            "http://localhost:3000/questions",
-            (data, status) => {
+
+        $.ajax({
+            url: "http://localhost:3000/questions",
+            type: "GET",
+            async: true,
+            success: (data) => {
+                tut = data
+
                 $.each(data, (i, e) => {
                     app(e)
                 })
+                total=data.length
             }
-        )//total list
-        $list.delegate(".remove", "click", () => {
-                console.log($(this).attr("data-id"))
-                $.ajax({
-                    url: "http://localhost:3000/questions/"+$(this).attr("data-id"),
-                    // url: "http://localhost:3000/questions/" +,
-                    type: "DELETE",
-                    success: (result) => {
-                        //add a successfully deleted line
-                    }
-                })
-        })//remove element
-
-        $(".ans").click(() => {
-            $.ajax({
-                url: `http://localhost:3000/questions/1`,
-                type: "GET",
-                success: (result) => {
-                    console.log("hhdhdh")
-                }
-            })
         })
 
+        function update(id){
+            $.ajax({
+                url: "http://localhost:3000/questions"+id,
+                type: "PUT",
+                async: true,
+                success: (data) => {
+                    app(data)
+                }
+            })   
+        }//put
 
+        function puttt(id,data){
+            $.ajax({
+                url: "http://localhost:3000/questions/"+id,
+                type: "PUT",
+                data:data,
+                success: () => {
+
+                }
+            })
+        }
+        $list.on('click', '.remove', (e) => {
+            $.ajax({
+                url: `http://localhost:3000/questions/${$(e.target).attr('data-id')}`,
+                type: "DELETE",
+                success: (c) => {
+                    $(this).parent("li").remove()
+                    // console.log(e.target)
+                    // console.log(c)
+                }
+            })
+        })//remove element
+        $list.on('click', '.ans', (e) => {
+            $.ajax({
+                url: `http://localhost:3000/questions/${$(e.target).attr('daid')}`,
+                type: "GET",
+                success: (data) => {
+                    $("li").append(data.answer)
+                }
+            })
+        })//remove element
+
+        // $(".ans").click(()=>{
+        //     console.log("fff")
+        //     $("#list").append(`khdanwdlnwdai`)
+        // })
+
+
+        $("#getscore").click(() => {
+            $("#score").append(`<p> your total score is 0</p>`)
+        })
+        // $list.on("click",".remove",() => {
+        //     $(this).closest("li").remove();
+        // })
     }
 )
