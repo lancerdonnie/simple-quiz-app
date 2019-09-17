6c0f0799228f0f0d15194a679ba7938eb704dbc8
lancerdonnie <jideadedejifirst@gmail.com>
2019-09-17 06:13:25 +0100

    added radio buttons

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 $(
     () => {
+        var tut = {}
         var $list = $("#list")
         function app(appe) {
             $list.append(
                 `<li>${appe["id"]})  ${appe["question"]}</li> 
-                <p>${appe["option1"]}</p> 
-                <p>${appe["option2"]}</p> 
-                <p>${appe["option3"]}</p> 
-                <p>${appe["option4"]}</p>
+                <input type="radio">${appe["option1"]}</input> 
+                <input type="radio">${appe["option2"]}</input> 
+                <input type="radio">${appe["option3"]}</input> 
+                <input type="radio">${appe["option4"]}</input>
                 <button>answer</button>
-                <button class="remove" data-id=${appe.id}>delete</button>`)
+                <button class="remove" id='vee${appe["id"]}'>delete</button>`)
         }
         $("#button1").click(() => {
             let obj = {
@@ -20,44 +21,60 @@ $(
                 option4: $("#option4").val(),
                 answer: $("#answer").val()
             }
-            $.post(
-                "http://localhost:3000/questions",
-                obj,
-                (data, status) => {
+
+            $.ajax({
+                url: "http://localhost:3000/questions",
+                type: "POST",
+                data: obj,
+                async: false,
+                success: (data) => {
+                    tut = data
                     app(data)
                 }
-            )
+            })
 
         })//submit
-        $.get(
-            "http://localhost:3000/questions",
-            (data, status) => {
+
+        $.ajax({
+            url: "http://localhost:3000/questions",
+            type: "GET",
+            async: false,
+            success: (data) => {
+                tut = data
                 $.each(data, (i, e) => {
                     app(e)
                 })
             }
-        )//total list
-        $list.delegate(".remove", "click", () => {
-                console.log($(this).attr("data-id"))
-                $.ajax({
-                    url: "http://localhost:3000/questions/"+$(this).attr("data-id"),
-                    // url: "http://localhost:3000/questions/" +,
-                    type: "DELETE",
-                    success: (result) => {
-                        //add a successfully deleted line
-                    }
-                })
-        })//remove element
-
-        $(".ans").click(() => {
-            $.ajax({
-                url: `http://localhost:3000/questions/1`,
-                type: "GET",
-                success: (result) => {
-                    console.log("hhdhdh")
-                }
-            })
         })
+        // console.log(tut)
+
+        $list.on("click",".remove",() => {
+            $(this).closest("li").remove();
+        })
+        // $list.delegate(".remove","click", () => {
+        //     var ttt=$(this)
+        //         console.log("DDD",$(this).attr("name"))
+
+        //         $.ajax({
+        //             url: "http://localhost:3000/questions/",
+        //             // url: "http://localhost:3000/questions/" +,
+        //             type: "DELETE",
+        //             success: (result) => {
+        //                 //add a successfully deleted line
+        //             }
+        //         })
+        // })//remove element
+        
+
+        // $(".ans").click(() => {
+        //     $.ajax({
+        //         url: `http://localhost:3000/questions/1`,
+        //         type: "GET",
+        //         success: (result) => {
+        //             console.log("hhdhdh")
+        //         }
+        //     })
+        // })
 
 
     }
