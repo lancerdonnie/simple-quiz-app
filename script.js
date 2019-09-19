05873f211c1b8378f60cb42bad4421cf365679c7
lancerdonnie <jideadedejifirst@gmail.com>
2019-09-19 06:07:42 +0100

    added load button, made adjustments to search, added page icon, did major styling

--- script.js
+++ script.js
@@ -4,45 +4,34 @@ $(
         var total = 0;
         var app = function (appe, element) {
             element.append(
-                `<li class="def${appe["id"]}">${appe["id"]}.  ${appe["question"]}
-                <input id="radio1" name="radio${appe["id"]}" anss="${appe["answer"]}" type="radio" value="1">${appe["option1"]}</input>
-                <input id="radio2" name="radio${appe["id"]}" anss="${appe["answer"]}" type="radio" value="2">${appe["option2"]}</input>
-                <input id="radio3" name="radio${appe["id"]}" anss="${appe["answer"]}" type="radio" value="3">${appe["option3"]}</input> 
-                <input id="radio4" name="radio${appe["id"]}" anss="${appe["answer"]}" type="radio" value="4">${appe["option4"]}</input>
-                <button class="ans" daid="${appe["id"]}">answer</button>
-                <button class="remove" data-id="${appe["id"]}">X</button></li>`)
+                `
+                <div  id="xx">
+                <p class="text-justify def${appe["id"]}">${appe["id"]}. ${appe["question"]}</p><button style="float:right;display:flex" vvv="x" class="remove btn btn-danger" data-id="${appe["id"]}">X</button>
+                <li><input class="form-radio" id="radio1" name="radio${appe["id"]}" anss="${appe["answer"]}" type="radio" value="1"></input>
+                <label for="radio1">${appe["option1"]}</label></li>
+                <li><input class="form-radio" id="radio2" name="radio${appe["id"]}" anss="${appe["answer"]}" type="radio" value="2"></input>
+                <label for="radio2">${appe["option2"]}</label></li>
+                <li><input class="form-radio" id="radio3" name="radio${appe["id"]}" anss="${appe["answer"]}" type="radio" value="3"></input>
+                <label for="radio3">${appe["option3"]}</label></li>
+                <li><input class="form-radio" id="radio4" name="radio${appe["id"]}" anss="${appe["answer"]}" type="radio" value="4"></input>
+                <label  for="radio4">${appe["option4"]}</label></li>
+                
+                <button style="margin-top: 7px;" class="ans btn btn-dark" daid="${appe["id"]}">answer</button>
+                </div></br>`)
         }
-        // <form id="myform">
-        //         </form>
-
-        $("#button5").click(() => {
-            // var eee=$("input[type='radio']:checked")
-            // eee.change(()=>{
-            //     eee.each($("input[type='radio']:checked"), (i, e) => {
-            //         console.log($(this));
-            //     })
-            // })
-            // console.log(eee.val())
-            // var f = []
-            //     $("#myform").each($("input[type='radio']:checked"), (i,e) => {
-            //        f.push()
-            //     })
-            //     console.log(f)
-
-        })
 
         $("#button4").click(() => {
             $("#div1").empty()
         })//clear
 
         $("#button3").click((e) => {
+            $("#div1").empty()
             let q = $("#search").val()
             $.ajax({
                 url: `http://localhost:3000/questions/${q}`,
                 type: "GET",
                 async: true,
                 success: (appe) => {
-                    // app(data)
                     $("#search").val("")
                     $("#div1").append(
                         `<li class="def${appe["id"]}">${appe["id"]})  ${appe["question"]}
@@ -59,10 +48,9 @@ $(
         })//search
         $("#edit").click(() => {
             $("#upda").toggleClass("hidden")
-        })
+        })//edit toggle
         $("#load").click(() => {
             let id = $("#id2").val()
-
             $.ajax({
                 url: `http://localhost:3000/questions/${id}`,
                 type: "GET",
@@ -74,12 +62,15 @@ $(
                     $("#option32").val(data.option3)
                     $("#option42").val(data.option4)
                     $("#answer2").val(data.option4)
+                },
+                error:()=>{
+                    alert("Please enter a valid number")
+                    $("#id2").val("")
                 }
             })
         })//load
 
         $("#button2").click(() => {
-
             let obj = {
                 id: $("#id2").val(),
                 question: $("#question2").val(),
@@ -93,19 +84,18 @@ $(
                 url: `http://localhost:3000/questions/${obj.id}`,
                 type: "PATCH",
                 data: obj,
-                async: true,
-                success: (data) => {
-                    $("#list").empty()
+                success: () => {
+                    var inp = $("#rdiv input")
+                    $.each(inp, ((x, y) => {
+                        $(y).val("")
+                    }))
                     $("#score p").empty()
-                    // app(data, $list)
+                    $("#list").empty()
+                    reloadQue()
                 }
             })
-            var inp = $("#rdiv input")
-            $.each(inp, ((x, y) => {
-                $(y).val("")
-            }))
-            reloadQue()
         })//update
+
         $("#button1").click(() => {
             let obj = {
                 question: $("#question").val(),
@@ -119,7 +109,6 @@ $(
                 url: "http://localhost:3000/questions",
                 type: "POST",
                 data: obj,
-                async: true,
                 success: (data) => {
                     app(data, $list)
                 }
@@ -148,60 +137,39 @@ $(
                     $(e.target).parent().remove()
                 }
             })
-        })//remove element
+        })//delete element
 
         $("body").on('click', '.ans', (e) => {
             $.ajax({
                 url: `http://localhost:3000/questions/${$(e.target).attr('daid')}`,
                 type: "GET",
                 success: (data) => {
-                    if ($(e.target).prev().attr("anss") == $(e.target).parent().children("input[type='radio']:checked").attr("value")) {
-                        $(e.target).parent().append(`The answer is correct `)
+                    if ($(e.target).prev().children("input").attr("anss") == $(e.target).parent().children("li").children("input[type='radio']:checked").attr("value")) {
+                        $(e.target).parent().append(`<span style="color:white;background-color:green"> The answer is correct </span>`)
                     } else {
-                        $(e.target).parent().append(`The answer is wrong. The correct is (option ${data.answer})`)
+                        $(e.target).parent().append(`<span style="color:white;background-color:red;">The answer is wrong. The correct is (option ${data.answer})</span>`)
                     }
                     $(e.target).parent().children().attr('disabled', true);
 
-                    // //if the clicked answer attr = answer provided
-                    // //It helpful to add .change() to the end to trigger any other events on the page
+                    $(e.target).parent().children().children().attr('disabled', true);
                 }
-
             })
-
-
         })//show answerS
 
-        $("body").on("change", "input[type='radio']", function (e) {
-            if ($(e.target).val() == $(e.target).attr("anss")) {
-                console.log("correct")
-                // total++
-            }
-            else {
-                console.log("wrong")
-            }
-            // $(e.target).attr("disabled",true)
-        });
-
-        // $(".ans").click(()=>{
-        //     console.log("fff")
-        //     $("#list").append(`khdanwdlnwdai`)
-        // })
-
-
         $("#getscore").click(() => {
-            var p = []
-            var $q = $("body li input[type='radio']:checked")
+            var $q = $("body #xx input[type='radio']:checked")
             $.each($q, (x, y) => {
                 if ($(y).attr("value") == $(y).attr("anss")) {
                     total++
                 }
             })
-            $("#score").append(`<p> your total score is ${total}</p>`)
+            $("#score").append(`<div><p style="background-color:#808285;display: inline;color:white"> Your total score is ${total}</p></div>`)
         })
+01
         $("#reset").click(() => {
             reloadQue()
         })//reset
-        function reloadQue(){
+        function reloadQue() {
             $("#list").empty()
             $("#score p").empty()
             $.ajax({
